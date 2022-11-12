3b447f575ccfd0d53fb6c148c81020ac51f83940
JanKaul <jankaul@mailbox.org>
2022-11-12 10:47:29 +0100

    update pkg

--- pkg/wasmer_wasi_js.d.ts
+++ pkg/wasmer_wasi_js.d.ts
@@ -176,6 +176,18 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
+  readonly __wbg_wasi_free: (a: number) => void;
+  readonly wasi_new: (a: number, b: number) => void;
+  readonly wasi_fs: (a: number, b: number) => void;
+  readonly wasi_getImports: (a: number, b: number, c: number) => void;
+  readonly wasi_instantiate: (a: number, b: number, c: number, d: number) => void;
+  readonly wasi_start: (a: number, b: number, c: number) => void;
+  readonly wasi_getStdoutBuffer: (a: number, b: number) => void;
+  readonly wasi_getStdoutString: (a: number, b: number) => void;
+  readonly wasi_getStderrBuffer: (a: number, b: number) => void;
+  readonly wasi_getStderrString: (a: number, b: number) => void;
+  readonly wasi_setStdinBuffer: (a: number, b: number, c: number, d: number) => void;
+  readonly wasi_setStdinString: (a: number, b: number, c: number, d: number) => void;
   readonly __wbg_wasmerruntimeerror_free: (a: number) => void;
   readonly __wbg_memfs_free: (a: number) => void;
   readonly memfs_new: (a: number) => void;
@@ -198,18 +210,6 @@ export interface InitOutput {
   readonly jsvirtualfile_writeString: (a: number, b: number, c: number, d: number) => void;
   readonly jsvirtualfile_flush: (a: number, b: number) => void;
   readonly jsvirtualfile_seek: (a: number, b: number, c: number) => void;
-  readonly __wbg_wasi_free: (a: number) => void;
-  readonly wasi_new: (a: number, b: number) => void;
-  readonly wasi_fs: (a: number, b: number) => void;
-  readonly wasi_getImports: (a: number, b: number, c: number) => void;
-  readonly wasi_instantiate: (a: number, b: number, c: number, d: number) => void;
-  readonly wasi_start: (a: number, b: number, c: number) => void;
-  readonly wasi_getStdoutBuffer: (a: number, b: number) => void;
-  readonly wasi_getStdoutString: (a: number, b: number) => void;
-  readonly wasi_getStderrBuffer: (a: number, b: number) => void;
-  readonly wasi_getStderrString: (a: number, b: number) => void;
-  readonly wasi_setStdinBuffer: (a: number, b: number, c: number, d: number) => void;
-  readonly wasi_setStdinString: (a: number, b: number, c: number, d: number) => void;
   readonly __wbg_lightningfs_free: (a: number) => void;
   readonly lightningfs_new: (a: number) => void;
   readonly canonical_abi_realloc: (a: number, b: number, c: number, d: number) => number;
@@ -218,9 +218,9 @@ export interface InitOutput {
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
   readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h97530bad69cd8bd0: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_exn_store: (a: number) => void;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
   readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
