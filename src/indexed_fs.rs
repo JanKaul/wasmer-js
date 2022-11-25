3b5098a2f9af1b9e84b67dd6190bfa32c656b7f4
JanKaul <jankaul@mailbox.org>
2022-11-25 07:59:23 +0100

    handle fs error

--- src/indexed_fs.rs
+++ src/indexed_fs.rs
@@ -14,29 +14,29 @@ extern "C" {
     #[derive(Debug)]
     pub type FS;
 
-    #[wasm_bindgen(method, js_name = mkdirSync)]
-    fn mkdir(this: &FS, filepath: String);
-    #[wasm_bindgen(method, js_name = rmdirSync)]
-    fn rmdir(this: &FS, filepath: String);
-    #[wasm_bindgen(method, js_name = readdirSync)]
-    fn readdir(this: &FS, filepath: String) -> js_sys::Array;
+    #[wasm_bindgen(method, catch, js_name = mkdirSync)]
+    fn mkdir(this: &FS, filepath: String) -> Result<(), JsValue>;
+    #[wasm_bindgen(method, catch , js_name = rmdirSync)]
+    fn rmdir(this: &FS, filepath: String) -> Result<(), JsValue>;
+    #[wasm_bindgen(method, catch , js_name = readdirSync)]
+    fn readdir(this: &FS, filepath: String) -> Result<js_sys::Array, JsValue>;
 
-    #[wasm_bindgen(method, js_name = writeFileSync)]
-    fn writeFile(this: &FS, filepath: String, data: js_sys::Uint8Array);
-    #[wasm_bindgen(method, js_name = unlinkSync)]
-    fn deleteFile(this: &FS, filepath: String);
-    #[wasm_bindgen(method, js_name = readFileSync)]
-    fn readFile(this: &FS, filepath: String) -> js_sys::Uint8Array;
-    #[wasm_bindgen(method, js_name = renameSync)]
-    fn rename(this: &FS, oldFilepath: String, newFilepath: String);
-    #[wasm_bindgen(method, js_name = statSync)]
-    fn stat(this: &FS, filepath: String) -> StatsLike;
-    #[wasm_bindgen(method, js_name = statSync)]
-    fn lstat(this: &FS, filepath: String) -> StatsLike;
+    #[wasm_bindgen(method, catch , js_name = writeFileSync)]
+    fn writeFile(this: &FS, filepath: String, data: js_sys::Uint8Array) -> Result<(), JsValue>;
+    #[wasm_bindgen(method, catch , js_name = unlinkSync)]
+    fn deleteFile(this: &FS, filepath: String) -> Result<(), JsValue>;
+    #[wasm_bindgen(method, catch , js_name = readFileSync)]
+    fn readFile(this: &FS, filepath: String) -> Result<js_sys::Uint8Array, JsValue>;
+    #[wasm_bindgen(method, catch , js_name = renameSync)]
+    fn rename(this: &FS, oldFilepath: String, newFilepath: String) -> Result<(), JsValue>;
+    #[wasm_bindgen(method, catch , js_name = statSync)]
+    fn stat(this: &FS, filepath: String) -> Result<StatsLike, JsValue>;
+    #[wasm_bindgen(method, catch , js_name = statSync)]
+    fn lstat(this: &FS, filepath: String) -> Result<StatsLike, JsValue>;
 
     type StatsLike;
 
-    #[wasm_bindgen(method, getter, js_name = type)]
+    #[wasm_bindgen(method , getter, js_name = type)]
     fn file_type(this: &StatsLike) -> String;
 }
 
@@ -65,7 +65,7 @@ impl IndexedFS {
 impl FileSystem for IndexedFS {
     fn read_dir(&self, path: &Path) -> Result<ReadDir, FsError> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        let array = get_fs()?.readdir(path.clone());
+        let array = get_fs()?.readdir(path.clone()).map_err(catchFsError)?;
         let data = array
             .iter()
             .map(|x| {
@@ -73,7 +73,9 @@ impl FileSystem for IndexedFS {
                 {
                     let name: js_sys::JsString = x.dyn_into().map_err(|_| FsError::UnknownError)?;
                     let name: String = format!("{}", name).into();
-                    let stats = get_fs()?.stat(move_path.clone() + "/" + &name);
+                    let stats = get_fs()?
+                        .stat(move_path.clone() + "/" + &name)
+                        .map_err(catchFsError)?;
                     Ok(DirEntry {
                         path: name.into(),
                         metadata: get_metadata(&stats.file_type()),
@@ -85,33 +87,51 @@ impl FileSystem for IndexedFS {
     }
     fn create_dir(&self, path: &Path) -> Result<(), FsError> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        Ok(get_fs()?.mkdir(path.to_string()))
+        get_fs()?.mkdir(path.to_string()).map_err(catchFsError)
     }
     fn remove_dir(&self, path: &Path) -> Result<(), FsError> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        Ok(get_fs()?.rmdir(path.to_string()))
+        get_fs()?.rmdir(path.to_string()).map_err(catchFsError)
     }
     fn rename(&self, from: &Path, to: &Path) -> Result<(), FsError> {
         let from = from.to_str().ok_or(FsError::UnknownError)?.to_string();
         let to = to.to_str().ok_or(FsError::UnknownError)?.to_string();
-        Ok(get_fs()?.rename(from.to_string(), to.to_string()))
+        get_fs()?
+            .rename(from.to_string(), to.to_string())
+            .map_err(catchFsError)
     }
     fn metadata(&self, path: &Path) -> Result<Metadata, FsError> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        let stats = get_fs()?.stat(path.to_string());
+        let stats = get_fs()?.stat(path.to_string()).map_err(catchFsError)?;
         get_metadata(&stats.file_type())
     }
     fn symlink_metadata(&self, path: &Path) -> Result<Metadata, FsError> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        let stats = get_fs()?.lstat(path.to_string());
+        let stats = get_fs()?.lstat(path.to_string()).map_err(catchFsError)?;
         get_metadata(&stats.file_type())
     }
     fn remove_file(&self, path: &Path) -> Result<(), FsError> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        Ok(get_fs()?.deleteFile(path.to_string()))
+        get_fs()?.deleteFile(path.to_string()).map_err(catchFsError)
     }
     fn new_open_options(&self) -> OpenOptions {
         OpenOptions::new(Box::new(IndexedFileOpener))
+    }
+}
+
+fn catchFsError(err: JsValue) -> FsError {
+    if let Ok(err) = err.dyn_into::<js_sys::Error>() {
+        if format!("{}", err.message()).starts_with("ENOENT") {
+            FsError::EntityNotFound
+        } else if format!("{}", err.message()).starts_with("ENOTDIR") {
+            FsError::BaseNotDirectory
+        } else if format!("{}", err.message()).starts_with("EISDIR") {
+            FsError::NotAFile
+        } else {
+            FsError::UnknownError
+        }
+    } else {
+        FsError::UnknownError
     }
 }
 
@@ -124,7 +144,8 @@ impl FileOpener for IndexedFileOpener {
         _conf: &wasmer_vfs::OpenOptionsConfig,
     ) -> wasmer_vfs::Result<Box<dyn VirtualFile + Send + Sync + 'static>> {
         let path = path.to_str().ok_or(FsError::UnknownError)?.to_string();
-        let data: js_sys::Uint8Array = get_fs()?.readFile(path.to_string());
+        let data: js_sys::Uint8Array =
+            get_fs()?.readFile(path.to_string()).map_err(catchFsError)?;
         let metadata = get_metadata("file")?;
         Ok(Box::new(IndexedVirtualFile {
             path: path.to_string(),
@@ -209,11 +230,14 @@ impl Write for IndexedVirtualFile {
         self.data.flush()?;
         let data = js_sys::Uint8Array::from(self.data.get_ref().as_ref());
         let temp_path = self.path.clone();
-        Ok(get_fs()
+        get_fs()
             .map_err(|_| {
                 std::io::Error::new(std::io::ErrorKind::ConnectionAborted, FsError::UnknownError)
             })?
-            .writeFile(temp_path, data))
+            .writeFile(temp_path, data)
+            .map_err(|err| {
+                std::io::Error::new(std::io::ErrorKind::ConnectionAborted, catchFsError(err))
+            })
     }
 }
 
@@ -245,6 +269,8 @@ impl VirtualFile for IndexedVirtualFile {
     }
 
     fn unlink(&mut self) -> Result<(), FsError> {
-        Ok(get_fs()?.deleteFile(self.path.to_string()))
+        get_fs()?
+            .deleteFile(self.path.to_string())
+            .map_err(catchFsError)
     }
 }
